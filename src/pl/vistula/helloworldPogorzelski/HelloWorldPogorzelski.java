458924ac43916647cd76b29f72e2f3f483e57645
Your Name <you@example.com>
2022-10-13 15:12:48 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldPogorzelski/HelloWorldPogorzelski.java
@@ -0,0 +1,8 @@
+package pl.vistula.helloworldPogorzelski;
+
+public class HelloWorldPogorzelski {
+    public static void main(String[] args) {
+        System.out.print("Hello Vistula Pogorzelski");
+        System.out.println("Hello World");
+    }
+}
